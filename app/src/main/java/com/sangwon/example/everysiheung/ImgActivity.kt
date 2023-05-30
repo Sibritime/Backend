f103a037bf17a40a658b206ce43e7455d7445ffd
Sangwon <qmfdnjssk@tukorea.ac.kr>
2023-05-30 11:37:18 +0900

    feat:임시 저장
    123

---
+++ app/src/main/java/com/sangwon/example/everysiheung/ImgActivity.kt
@@ -0,0 +1,11 @@
+package com.sangwon.example.everysiheung
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ImgActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_img)
+    }
+}

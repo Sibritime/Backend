18c7ec6ae9913d3f45af33fe6391e319332c9d8b
Sangwon <qmfdnjssk@tukorea.ac.kr>
2023-05-31 23:51:31 +0900

    feat : 이미지 저장
    이미지 주소 가져와서 imageView에 띄우기

--- app/src/main/java/com/sangwon/example/everysiheung/ImgActivity.kt
+++ app/src/main/java/com/sangwon/example/everysiheung/ImgActivity.kt
@@ -1,5 +1,6 @@
 package com.sangwon.example.everysiheung
 
+import android.app.Activity
 import android.content.Intent
 import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
@@ -11,6 +12,7 @@ import android.widget.Button
 import android.widget.ImageView
 import android.widget.ProgressBar
 import android.widget.Toast
+import androidx.appcompat.widget.AppCompatButton
 import com.google.firebase.firestore.DocumentReference
 import com.google.firebase.firestore.FirebaseFirestore
 import com.google.firebase.firestore.ktx.firestore
@@ -23,25 +25,17 @@ class ImgActivity : AppCompatActivity() {
 
     private lateinit var imageView: ImageView
     private lateinit var progressBar: ProgressBar
-    private val root = FirebaseFirestore.getInstance().collection("Image")
+    private lateinit var downloadUri: String
     val db = Firebase.firestore
     //공식 문서 드가자
     val storage = Firebase.storage
     val storageRef = storage.reference
-    val mountainsRef = storageRef.child("mountains.jpg")
-    val mountainImagesRef = storageRef.child("images/mountains.jpg")
-
-    // val reference = FirebaseStorage.getInstance().getReference()를 사용하여 Firebase Storage의 루트 참조를 얻습니다.
-    private val reference = FirebaseStorage.getInstance().getReference()
-
-
     private lateinit var imageUri: Uri
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_img)
 
-        // Get the image view and progress bar
         imageView = findViewById(R.id.image_view)
         progressBar = findViewById(R.id.progress_View)
 
@@ -64,19 +58,20 @@ class ImgActivity : AppCompatActivity() {
         }
 
         // Set an onclick listener for the upload button
-        findViewById<Button>(R.id.upload_btn).setOnClickListener {
+        findViewById<AppCompatButton>(R.id.upload_btn).setOnClickListener {
 
             // Check if an image is selected
             if (imageUri != null) {
 
-                // Upload the image to Firestore
-                // 밑에 함수 정의함
                 uploadToFirestore(imageUri)
+
 
             } else {
                 Toast.makeText(this, "Please select an image.", Toast.LENGTH_SHORT).show()
             }
         }
+
+
     }
 
     // 갤러리에서 사진 받으면 실행하는 함수
@@ -88,12 +83,12 @@ class ImgActivity : AppCompatActivity() {
 
             // Get the image Uri
             imageUri = data.data!!
+            //Log.e("img","${imageUri}")
+            //content://com.android.providers.media.documents/document/image%3A13
 
             // Set the image Uri to the image view
             imageView.setImageURI(imageUri)
-            //아주 뭔 이상한 값을 가지고 있네
-            //content://com.google.android.apps.photos.contentprovider/-1/1/content%3A%2F%2Fmedia%2Fexternal%2Fimages%2Fmedia%2F13/ORIGINAL/NONE/image%2Fjpeg/273006428
-            //Log.e("img","${imageUri}")
+
         }
     }
 
@@ -102,42 +97,21 @@ class ImgActivity : AppCompatActivity() {
 
         // Get the storage reference
         // Firebase Storage에서 사용할 파일 참조를 생성합니다. 파일 참조의 경로는 현재 시간과 이미지 파일의 확장자를 조합하여 생성됩니다.
-        val fileRef = reference.child(System.currentTimeMillis().toString() + "." + getFileExtension(imageUri))
-
-        val ref = storageRef.child("images/mountains.jpg")
-        var uploadTask = ref.putFile(imageUri)
-        uploadTask.continueWithTask {
-            if (!it.isSuccessful) {
-                it.exception?.let {
-                    throw it
-                }
-            }
-            ref.downloadUrl
-        }.addOnCompleteListener { task ->
-            if (task.isSuccessful) {
-                val downloadUri = task.result
-            } else {
-                // Handle failures
-                // ...
-            }
-        }
-
-
-
-
-
-
-
-
-
+        var imageName = System.currentTimeMillis().toString() + "." + getFileExtension(imageUri)
+        val fileref = storageRef.child("images/${imageName}")
 
         // Upload the image
-        fileRef.putFile(imageUri)
+        fileref.putFile(imageUri)
             .addOnSuccessListener { taskSnapshot ->
 
                 // Get the download Uri
-                val downloadUri = taskSnapshot.storage.downloadUrl
-                Log.e("uri","${downloadUri}")
+                val imagePath = "images/${imageName}"
+
+                var outIntent = Intent(applicationContext, ImgActivity::class.java)
+                outIntent.putExtra("imagePath", imagePath)
+
+                setResult(RESULT_OK, outIntent)
+
 
 //                // Create a document
 //                val document = DocumentReference.create(root, downloadUri.toString())
@@ -145,14 +119,7 @@ class ImgActivity : AppCompatActivity() {
 //                // Set the data
 //                document.set(mapOf("imageUri" to downloadUri.toString()))
 
-                db.collection("Images")
-                    .add(mapOf("imageUri" to downloadUri.toString()))
-                    .addOnSuccessListener { documentReference ->
-                        Log.d("db", "DocumentSnapshot added with ID: ${documentReference.id}")
-                    }
-                    .addOnFailureListener { e ->
-                        Log.w("db", "Error adding document", e)
-                    }
+
 
                 // Hide the progress bar
                 progressBar.visibility = View.INVISIBLE
@@ -162,6 +129,9 @@ class ImgActivity : AppCompatActivity() {
 
                 // Set the image resource to the image view
                 imageView.setImageResource(R.drawable.ic_add_photo)
+                Thread.sleep(1000)
+                finish()
+
             }
             .addOnProgressListener { snapshot ->
 

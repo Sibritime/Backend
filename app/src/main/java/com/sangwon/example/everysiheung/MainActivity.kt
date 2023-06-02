a37934393a1a304fd7dfac9983132ae92eb375c5
Shin Hyuk Jun <tlsgurwns123@naver.com>
2023-06-02 12:41:56 +0900

    feat: 6월 포스터_터치하면 상세페이지 넘어감.
    6월 포스터 업데이트, 포스터 터치하면 상세페이지 넘어감.

--- app/src/main/java/com/sangwon/example/everysiheung/MainActivity.kt
+++ app/src/main/java/com/sangwon/example/everysiheung/MainActivity.kt
@@ -22,6 +22,7 @@ import androidx.recyclerview.widget.SnapHelper
 import androidx.viewpager2.widget.ViewPager2
 import com.google.android.material.navigation.NavigationView
 import com.sangwon.example.everysiheung.adapter.CalendarAdapter
+import com.sangwon.example.everysiheung.adapter.ImageData
 import com.sangwon.example.everysiheung.adapter.ViewPagerAdapter
 import com.sangwon.example.everysiheung.databinding.ActivityMainBinding
 import com.sangwon.example.everysiheung.model.CalendarDateModel
@@ -53,13 +54,68 @@ class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelecte
     private lateinit var adapter: CalendarAdapter
     private val calendarList2 = ArrayList<CalendarDateModel>()
 
-    private var numBanner = 3 // 배너 갯수
-    private var currentPosition = Int.MAX_VALUE / numBanner
+    private val idolList: ArrayList<ImageData> = getIdolList()
+    private val numBanner: Int = idolList.size
+    private var currentPosition = numBanner * 1000
+
     private var myHandler = MyHandler()
     private val intervalTime = 5000.toLong() // 몇초 간격으로 페이지를 넘길것인지 (1500 = 1.5초)
 
     private lateinit var navigationView: NavigationView
     private lateinit var drawerLayout: DrawerLayout
+
+    /**
+     * (230602기준) 6월 포스터 표시됨. 이미지 데이터 추가해서 포스터 추가 가능.
+     */
+    private fun getIdolList(): ArrayList<ImageData> {
+        val idolList = ArrayList<ImageData>()
+
+        val imageData1 = ImageData("https://www.siheung.go.kr/cmm/fms/FileDown.do?atchFileId=148c15d19a358e7fd81799db36f4771c42076e7d19cdb7974115393f2eb97c1a&fileSn=f9a1967c526603d17ab488b9d2747cda",
+            "https://www.siheung.go.kr/event/main.do?idx=6277&year=2023&month=6&stateFlag=calendar")
+        val imageData2 = ImageData("https://www.siheung.go.kr/cmm/fms/FileDown.do?atchFileId=148c15d19a358e7fd81799db36f4771c91995d2aad34f5a69ce6a53d0e720bb6&fileSn=f9a1967c526603d17ab488b9d2747cda",
+            "https://www.siheung.go.kr/event/main.do?idx=6276&year=2023&month=6&stateFlag=calendar")
+        val imageData3 = ImageData("https://www.siheung.go.kr/cmm/fms/FileDown.do?atchFileId=148c15d19a358e7fd81799db36f4771c37415ce797b610c5d717a5b8e6d87f74&fileSn=f9a1967c526603d17ab488b9d2747cda",
+            "https://www.siheung.go.kr/event/main.do?idx=6275&year=2023&month=6&stateFlag=calendar")
+        val imageData4 = ImageData("https://www.siheung.go.kr/cmm/fms/FileDown.do?atchFileId=148c15d19a358e7fd81799db36f4771c23d5d53603818a0c7bc591e475634fbc&fileSn=f9a1967c526603d17ab488b9d2747cda",
+            "https://www.siheung.go.kr/event/main.do?idx=6215&year=2023&month=6&stateFlag=calendar")
+        val imageData5 = ImageData("https://www.siheung.go.kr/cmm/fms/FileDown.do?atchFileId=148c15d19a358e7fd81799db36f4771c7564eebd039cc288b9ce14bedce381ab&fileSn=f9a1967c526603d17ab488b9d2747cda",
+            "https://www.siheung.go.kr/event/main.do?idx=6098&year=2023&month=6&stateFlag=calendar")
+        val imageData6 = ImageData("https://www.siheung.go.kr/cmm/fms/FileDown.do?atchFileId=148c15d19a358e7fd81799db36f4771cab62a6bdb36afc5d7135aef1ef6c8580&fileSn=f9a1967c526603d17ab488b9d2747cda",
+            "https://www.siheung.go.kr/event/main.do?idx=6304&year=2023&month=6&stateFlag=calendar")
+        val imageData7 = ImageData("https://www.siheung.go.kr/cmm/fms/FileDown.do?atchFileId=148c15d19a358e7fd81799db36f4771c676b5464ed1b6ea919802b8a931af345&fileSn=f9a1967c526603d17ab488b9d2747cda",
+            "https://www.siheung.go.kr/event/main.do?idx=6251&year=2023&month=6&stateFlag=calendar")
+        val imageData8 = ImageData("https://www.siheung.go.kr/cmm/fms/FileDown.do?atchFileId=148c15d19a358e7fd81799db36f4771c8972f79b8c4c374bf9e4420bc35a0f6c&fileSn=f9a1967c526603d17ab488b9d2747cda",
+            "https://www.siheung.go.kr/event/main.do?idx=6133&year=2023&month=6&stateFlag=calendar")
+        val imageData9 = ImageData("https://www.siheung.go.kr/cmm/fms/FileDown.do?atchFileId=148c15d19a358e7fd81799db36f4771c39ea3d9ce5696b8dc08e7dd5aadcfbae&fileSn=3743fa93046d4584eb9e93f9fe62c66e",
+            "https://www.siheung.go.kr/event/main.do?idx=6115&year=2023&month=6&stateFlag=calendar")
+        val imageData10 = ImageData("https://www.siheung.go.kr/cmm/fms/FileDown.do?atchFileId=148c15d19a358e7fd81799db36f4771c87abc44a2384319b02953a94634a14b4&fileSn=f9a1967c526603d17ab488b9d2747cda",
+            "https://www.siheung.go.kr/event/main.do?idx=6062&year=2023&month=6&stateFlag=calendar")
+        val imageData11 = ImageData("https://www.siheung.go.kr/cmm/fms/FileDown.do?atchFileId=148c15d19a358e7fd81799db36f4771c2f754a6fb67d873b1c3245380485b46b&fileSn=f9a1967c526603d17ab488b9d2747cda",
+            "https://www.siheung.go.kr/event/main.do?idx=6111&year=2023&month=6&stateFlag=calendar")
+        val imageData12 = ImageData("https://www.siheung.go.kr/cmm/fms/FileDown.do?atchFileId=148c15d19a358e7fd81799db36f4771ce730bf53712b80a331263ef6e9d56931&fileSn=f9a1967c526603d17ab488b9d2747cda",
+            "https://www.siheung.go.kr/event/main.do?idx=6075&year=2023&month=6&stateFlag=calendar")
+
+        idolList.add(imageData1)
+        idolList.add(imageData2)
+        idolList.add(imageData3)
+        idolList.add(imageData4)
+        idolList.add(imageData5)
+        idolList.add(imageData6)
+        idolList.add(imageData7)
+        idolList.add(imageData8)
+        idolList.add(imageData9)
+        idolList.add(imageData10)
+        idolList.add(imageData11)
+        idolList.add(imageData12)
+
+        return idolList
+    }
+
+    //추가한 내용
+    /*private fun getIdolList(): ArrayList<String> {
+        //return arrayListOf<Int>(R.drawable.pic1, R.drawable.pic2, R.drawable.pic3)
+        return arrayListOf<String>("https://www.siheung.go.kr/common/imgView.do?attachId=148c15d19a358e7fd81799db36f4771c6111c4314f80b6b967aa9fccff04d2e1&fileSn=f9a1967c526603d17ab488b9d2747cda&mode=origin","https://www.siheung.go.kr/common/imgView.do?attachId=148c15d19a358e7fd81799db36f4771c42076e7d19cdb7974115393f2eb97c1a&fileSn=f9a1967c526603d17ab488b9d2747cda&mode=origin","https://www.siheung.go.kr/common/imgView.do?attachId=148c15d19a358e7fd81799db36f4771c7564eebd039cc288b9ce14bedce381ab&fileSn=f9a1967c526603d17ab488b9d2747cda&mode=origin")
+    }*/
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -95,13 +151,15 @@ class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelecte
         binding.posterViewpager.orientation = ViewPager2.ORIENTATION_HORIZONTAL
         binding.posterViewpager.setCurrentItem(currentPosition, false) // 현재 위치를 지정
         binding.posterViewpager.setPageTransformer(ZoomOutPageTransformer())
+        binding.textViewBanner.text = "[${(currentDate.get(Calendar.MONTH) + 1)}월 행사 포스터]   1 / $numBanner"
+
 
         // 현재 몇번째 배너인지 보여주는 숫자를 변경함
         binding.posterViewpager.apply {
             registerOnPageChangeCallback(object : ViewPager2.OnPageChangeCallback() {
                 override fun onPageSelected(position: Int) {
                     super.onPageSelected(position)
-                    binding.textViewBanner.text = "[${(currentDate.get(Calendar.MONTH) + 1)}월 행사 포스터]   ${(position % numBanner) + 1} / $numBanner" // 하드코딩. 현재 포스터 수는 3개이다.
+                    binding.textViewBanner.text = "[${(currentDate.get(Calendar.MONTH) + 1)}월 행사 포스터]   ${(position % numBanner) + 1} / $numBanner"
                 }
 
                 override fun onPageScrollStateChanged(state: Int) {
@@ -229,11 +287,7 @@ class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelecte
         autoScrollStop()
     }
 
-    //추가한 내용
-    private fun getIdolList(): ArrayList<String> {
-        //return arrayListOf<Int>(R.drawable.pic1, R.drawable.pic2, R.drawable.pic3)
-        return arrayListOf<String>("https://www.siheung.go.kr/common/imgView.do?attachId=148c15d19a358e7fd81799db36f4771c6111c4314f80b6b967aa9fccff04d2e1&fileSn=f9a1967c526603d17ab488b9d2747cda&mode=origin","https://www.siheung.go.kr/common/imgView.do?attachId=148c15d19a358e7fd81799db36f4771c42076e7d19cdb7974115393f2eb97c1a&fileSn=f9a1967c526603d17ab488b9d2747cda&mode=origin","https://www.siheung.go.kr/common/imgView.do?attachId=148c15d19a358e7fd81799db36f4771c7564eebd039cc288b9ce14bedce381ab&fileSn=f9a1967c526603d17ab488b9d2747cda&mode=origin")
-    }
+
     private fun moveTable() {
         val url = "https://www.siheung.go.kr/event/main.do?stateFlag=list"
         val intent = Intent(this, TableActivity::class.java)
--- app/src/main/java/com/sangwon/example/everysiheung/adapter/ViewPagerAdapter.kt
+++ app/src/main/java/com/sangwon/example/everysiheung/adapter/ViewPagerAdapter.kt
@@ -1,14 +1,19 @@
 package com.sangwon.example.everysiheung.adapter
 
+import android.content.Context
+import android.content.Intent
 import android.net.Uri
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import android.widget.ImageView
+import android.widget.Toast
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
 import com.sangwon.example.everysiheung.R
+import com.sangwon.example.everysiheung.TableActivity
 
-class ViewPagerAdapter(idolList: ArrayList<String>) : RecyclerView.Adapter<ViewPagerAdapter.PagerViewHolder>() {
+
+/*class ViewPagerAdapter(idolList: ArrayList<String>) : RecyclerView.Adapter<ViewPagerAdapter.PagerViewHolder>() {
     var item = idolList
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int) = PagerViewHolder(parent)
@@ -20,9 +25,44 @@ class ViewPagerAdapter(idolList: ArrayList<String>) : RecyclerView.Adapter<ViewP
         //holder.poster.setImageURI(Uri.parse(item[position%item.size])) // 포스터 수 만큼 하드코딩해야함. 지금은 포스터 2개인 상태
     }
 
+
+
     inner class PagerViewHolder(val parent: ViewGroup) : RecyclerView.ViewHolder
         (LayoutInflater.from(parent.context).inflate(R.layout.poster_list, parent, false)){
 
         val poster: ImageView = itemView.findViewById(R.id.imageView_poster)
+    }
+}*/
+
+// 데이터 모델 클래스
+data class ImageData(val imageUrl: String, val url: String)
+
+class ViewPagerAdapter(idolList: ArrayList<ImageData>) : RecyclerView.Adapter<ViewPagerAdapter.PagerViewHolder>() {
+    var item = idolList
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int) = PagerViewHolder(parent)
+
+    override fun getItemCount(): Int = Int.MAX_VALUE
+
+    override fun onBindViewHolder(holder: PagerViewHolder, position: Int) {
+        val realPosition = position % item.size
+        val imageData = item[realPosition]
+        Glide.with(holder.parent).load(imageData.imageUrl).into(holder.poster)
+        holder.itemView.setOnClickListener {
+            openUrl(holder.itemView.context, imageData.url)
+        }
+    }
+
+    inner class PagerViewHolder(val parent: ViewGroup) : RecyclerView.ViewHolder
+        (LayoutInflater.from(parent.context).inflate(R.layout.poster_list, parent, false)) {
+
+        val poster: ImageView = itemView.findViewById(R.id.imageView_poster)
+    }
+
+    private fun openUrl(context: Context, url: String) {
+        val intent = Intent(context, TableActivity::class.java)
+        intent.putExtra("url", url)
+        context.startActivity(intent)
+        Toast.makeText(context, "페이지 로드 중...", Toast.LENGTH_LONG).show()
     }
 }

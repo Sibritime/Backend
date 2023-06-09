28679c276daca24be133bf99e654b9e7d4711afa
Sangwon <qmfdnjssk@tukorea.ac.kr>
2023-06-09 14:50:22 +0900

    feat : 북마크 페이지
    북마크 누르면 누른 부분만 나오게끔

--- app/src/main/java/com/sangwon/example/everysiheung/PostBoardActivity.kt
+++ app/src/main/java/com/sangwon/example/everysiheung/PostBoardActivity.kt
@@ -5,6 +5,8 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.widget.ListView
+import android.widget.Toast
+import androidx.constraintlayout.widget.Placeholder
 import com.google.android.material.floatingactionbutton.FloatingActionButton
 import com.google.firebase.auth.ktx.auth
 import com.google.firebase.firestore.Query
@@ -36,8 +38,15 @@ class PostBoardActivity : AppCompatActivity() {
             val intent: Intent = Intent(this, PostUpActivity::class.java)
             startActivity(intent)
         }
-
-        addPostList()
+        //어떤 함수 사용해야 하는지 정하기
+        val intent = intent
+        if (intent.getStringExtra("Role") == "Posts") {
+            addPostList()
+        }
+        else if (intent.getStringExtra("Role") == "BookMarks")
+        {
+            BookMarksList()
+        }
 
         listview.setOnItemClickListener { adapterView, view, i, l ->
             val item = adapterView.getItemAtPosition(i) as PostItem
@@ -116,6 +125,84 @@ class PostBoardActivity : AppCompatActivity() {
                         if (postItems.size == result.size()) {
                             for (item in postItems) {
                                 adapter.addPost(item)
+                            }
+                            adapter.notifyDataSetChanged()
+                        }
+                    } else {
+                        Log.e("downloadUrl", "failed..")
+                    }
+                }
+            }
+        }
+    }
+
+    private fun BookMarksList() {
+        val firebaseStorage = FirebaseStorage.getInstance()
+        var imagePath: String = ""
+
+        GlobalScope.launch(Dispatchers.Main) {
+            val postItems = arrayListOf<PostItem?>() // 데이터를 임시로 저장할 리스트
+
+            val result = withContext(Dispatchers.IO) {
+                db.collection("Posts")
+                    .orderBy("timestamp", Query.Direction.DESCENDING)
+                    .get()
+                    .await()
+            }
+            for (document in result) {
+                Log.e("document","${document.id}")
+            }
+
+            for (document in result) {
+                val title = document.getString("title")
+                val location = document.getString("location")
+                val date = document.getString("date")
+                val id = document.id
+
+                //Timestamp(seconds=1686128427, nanoseconds=894000000)
+                val timestamp = document.getTimestamp("timestamp")// 2023년 6월 2일 오전 11시 15분 31초 UTC+9
+                Log.e("timestamp","${timestamp}")
+                imagePath = document.getString("image").toString()
+                Log.e("order", "${title}")
+                // 이미지를 등록하지 않은 경우 default 이미지
+                if (imagePath == "") {
+                    imagePath = "images/default.png"
+                }
+                Log.e("imgPath", "${imagePath}")
+                val storageReference = firebaseStorage.getReference().child(imagePath.toString())
+
+                val isFavorites = withContext(Dispatchers.IO) {
+                    val bookmarkQuerySnapshot = db.collection("MyPage")
+                        .document("${Firebase.auth.currentUser?.uid}")
+                        .collection("BookMarks")
+                        .get()
+                        .await()
+
+                    bookmarkQuerySnapshot.documents.any { it.id == id }
+                }
+
+                storageReference.downloadUrl.addOnCompleteListener { task ->
+                    if (task.isSuccessful) {
+                        val imageData = task.result
+                        val postItem = PostItem(
+                            img = imageData,
+                            title = title ?: "",
+                            location = location ?: "",
+                            date = date ?: "",
+                            time = "18:00~20:00",
+                            isFavorites = isFavorites,
+                            id = id
+                        )
+                        if (isFavorites == true) {
+                            postItems.add(postItem)
+                        }
+                        else{
+                            postItems.add(null)
+                        }
+                        // 모든 데이터를 가져왔을 때 어댑터에 추가하고 화면 업데이트
+                        if (postItems.size == result.size()) {
+                            for (item in postItems) {
+                                item?.let { adapter.addPost(it) } //널이 아니면 넣는다
                             }
                             adapter.notifyDataSetChanged()
                         }

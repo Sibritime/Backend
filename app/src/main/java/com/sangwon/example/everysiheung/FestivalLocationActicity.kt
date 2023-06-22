a047e5d15b045ebea4c9da2aa0bebcd06c403c55
Sangwon <qmfdnjssk@tukorea.ac.kr>
2023-06-23 03:56:22 +0900

    feat: 새벽의 저주

--- app/src/main/java/com/sangwon/example/everysiheung/FestivalLocationActicity.kt
+++ app/src/main/java/com/sangwon/example/everysiheung/FestivalLocationActicity.kt
@@ -1,10 +1,18 @@
 package com.sangwon.example.everysiheung
 
+import android.Manifest
 import android.annotation.SuppressLint
+import android.content.pm.PackageManager
+import android.graphics.Bitmap
+import android.graphics.BitmapFactory
+import android.graphics.Canvas
+import android.graphics.Paint
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.widget.RelativeLayout
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import com.google.firebase.firestore.Query
 import com.google.firebase.firestore.ktx.firestore
 import com.google.firebase.ktx.Firebase
@@ -18,6 +26,7 @@ class FestivalLocationActicity : AppCompatActivity(), MapView.MapViewEventListen
     private lateinit var mapView: MapView
     var storage = Firebase.storage
     val db = Firebase.firestore
+    private var LOCATION_PERMISSION_REQUEST_CODE = 2
 
     @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -31,6 +40,22 @@ class FestivalLocationActicity : AppCompatActivity(), MapView.MapViewEventListen
         mapView.setMapViewEventListener(this)
         mapView.setPOIItemEventListener(this)
 
+        // 마커의 크기를 증가시킬 비율
+        val markerSizeRatio = 0.05f
+
+        // 마커 이미지 리소스 ID
+        val markerImageResourceId = R.drawable.bluepin
+
+        // 마커 이미지를 비율에 맞게 크기 조정
+        val originalBitmap = BitmapFactory.decodeResource(resources, markerImageResourceId)
+        val scaledBitmap = Bitmap.createScaledBitmap(originalBitmap, (originalBitmap.width * markerSizeRatio).toInt(), (originalBitmap.height * markerSizeRatio).toInt(), false)
+
+        // 증가된 크기의 마커 이미지로 사용할 비트맵을 생성
+        val customMarker = Bitmap.createBitmap(scaledBitmap.width, scaledBitmap.height, Bitmap.Config.ARGB_8888)
+        val canvas = Canvas(customMarker)
+        val paint = Paint(Paint.ANTI_ALIAS_FLAG or Paint.DITHER_FLAG)
+        canvas.drawBitmap(scaledBitmap, 0f, 0f, paint)
+
         db.collection("Posts")
             .orderBy("timestamp", Query.Direction.DESCENDING)
             .get()
@@ -40,15 +65,17 @@ class FestivalLocationActicity : AppCompatActivity(), MapView.MapViewEventListen
                     var latitude = document.getDouble("latitude")?.toDouble()
                     var longitude = document.getDouble("longitude")?.toDouble()
 
+
                     if (latitude != null && longitude != null ) {
                         var mapPoint = MapPoint.mapPointWithGeoCoord(latitude, longitude)
 
                         var marker = MapPOIItem()
                         marker.itemName = "${title}"
+                        marker.tag = 0
                         marker.mapPoint = mapPoint
-                        marker.markerType = MapPOIItem.MarkerType.BluePin // 마커 아이콘 타입 설정
-                        marker.selectedMarkerType = MapPOIItem.MarkerType.RedPin // 선택된 마커 아이콘 타입 설정
-
+                        marker.markerType = MapPOIItem.MarkerType.CustomImage
+                        marker.selectedMarkerType = MapPOIItem.MarkerType.CustomImage
+                        marker.customImageBitmap = customMarker
                         mapView.addPOIItem(marker)
                         mapView.setMapCenterPoint(mapPoint, true)
                     }
@@ -58,7 +85,39 @@ class FestivalLocationActicity : AppCompatActivity(), MapView.MapViewEventListen
             .addOnFailureListener { exception ->
                 Log.d("PostInfoFailed", "Error getting documents: ", exception)
             }
+
+        requestLocationPermission()
     }
+    private fun requestLocationPermission() {
+        val permission = Manifest.permission.ACCESS_FINE_LOCATION
+        val permissionGranted = PackageManager.PERMISSION_GRANTED
+        if (ContextCompat.checkSelfPermission(this, permission) != permissionGranted) {
+            ActivityCompat.requestPermissions(this, arrayOf(permission), LOCATION_PERMISSION_REQUEST_CODE)
+        } else {
+            // 위치 권한이 이미 허용된 경우, 현재 위치로 지도 이동
+            moveMapToCurrentLocation()
+        }
+    }
+    private fun moveMapToCurrentLocation() {
+        mapView.currentLocationTrackingMode = MapView.CurrentLocationTrackingMode.TrackingModeOnWithoutHeading
+        mapView.setCustomCurrentLocationMarkerTrackingImage(R.drawable.custom_marker)
+
+    }
+    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        if (requestCode == LOCATION_PERMISSION_REQUEST_CODE) {
+            if (grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                // 위치 권한이 허용된 경우, 현재 위치로 지도 이동
+                moveMapToCurrentLocation()
+            } else {
+                // 위치 권한이 거부된 경우, 사용자에게 메시지 표시 또는 다른 동작 수행
+            }
+        }
+    }
+
+
+
+
 
 
 
@@ -98,4 +157,8 @@ class FestivalLocationActicity : AppCompatActivity(), MapView.MapViewEventListen
     override fun onCalloutBalloonOfPOIItemTouched(p0: MapView?, p1: MapPOIItem?) {}
     override fun onCalloutBalloonOfPOIItemTouched(p0: MapView?, p1: MapPOIItem?, p2: MapPOIItem.CalloutBalloonButtonType?, ) {}
     override fun onDraggablePOIItemMoved(p0: MapView?, p1: MapPOIItem?, p2: MapPoint?) {}
+}
+
+private fun MapView.setCustomCurrentLocationMarkerTrackingImage(customMarker: Int) {
+
 }
